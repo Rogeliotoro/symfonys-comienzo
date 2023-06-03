0e1272eb182cda0cd5e6ce974c6c4098c3b643d1
Rogeliotoro <rogeltoro@gmail.com>
2023-06-03 17:52:06 +0200

    get and post of podcastController

--- src/Controller/PodcastController.php
+++ src/Controller/PodcastController.php
@@ -2,18 +2,38 @@
 
 namespace App\Controller;
 
+use App\Entity\Podcast;
+use App\Repository\PodcastRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PodcastController extends AbstractController
 {
-    #[Route('/podcast', name: 'app_podcast' , methods:['GET'])]
-    public function index(): JsonResponse
+    #[Route('/podcast', name: 'podcast_list' , methods:['GET'])]
+    public function index(PodcastRepository $podcastRepository): JsonResponse
     {
         return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/PodcastController.php',
+            'data' => $podcastRepository->findAll(),
         ]);
+    }
+    #[Route('/podcast', name: 'podcast_create' , methods:['POST'])]
+    public function create(Request $request , PodcastRepository $podcastRepository): JsonResponse
+    {
+        $data = $request->request->all();
+        $podcast = new Podcast();
+        $podcast->setTitle($data['title']);
+     
+        $podcast->setUploadDate (new \DateTimeImmutable ('Europe/Madrid'));
+        $podcast->setDescription($data['description']);
+
+        $podcastRepository->save($podcast, true);
+
+        return $this->json([
+            'message' => 'Podcast create succesfully!',
+            'data' => $podcast
+            
+        ],201);
     }
 }
