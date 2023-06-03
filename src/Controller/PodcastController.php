c2be514035f21a553ed98d888358e4f0d2b55418
Rogeliotoro <rogeltoro@gmail.com>
2023-06-03 18:50:28 +0200

    update and delete podcastController- finish

--- src/Controller/PodcastController.php
+++ src/Controller/PodcastController.php
@@ -8,6 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
 
 class PodcastController extends AbstractController
 {
@@ -16,6 +17,17 @@ class PodcastController extends AbstractController
     {
         return $this->json([
             'data' => $podcastRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/podcast/{podcast}', name: 'podcast_single' , methods:['GET'])]
+    public function single(int $podcast, PodcastRepository $podcastRepository): JsonResponse
+    {
+        $podcast=  $podcastRepository->find($podcast);
+
+        if(!$podcast) throw $this->createNotFoundException();
+        return $this->json([
+            'data' => $podcast,
         ]);
     }
     #[Route('/podcast', name: 'podcast_create' , methods:['POST'])]
@@ -35,5 +47,36 @@ class PodcastController extends AbstractController
             'data' => $podcast
             
         ],201);
+    }
+    #[Route('/podcast/{podcast}', name: 'podcast_update' , methods:['PUT','PATCH'])]
+    public function update(int $podcast, Request $request , ManagerRegistry $doctrine , PodcastRepository $podcastRepository): JsonResponse
+    {
+    
+        $podcast = $podcastRepository->find($podcast);
+        if(!$podcast) throw $this->createNotFoundException();
+
+        $data = $request->request->all();
+        $podcast->setTitle($data['title']);
+        $podcast->setUploadDate (new \DateTimeImmutable ('Europe/Madrid'));
+        $podcast->setDescription($data['description']);
+
+        $doctrine->getManager()->flush();
+
+        return $this->json([
+            'message' => 'Podcast update succesfully!',
+            'data' => $podcast
+            
+        ],201);
+    }
+
+    #[Route('/podcast/{podcast}', name: 'podcast_update' , methods:['DELETE'])]
+    public function delete(int $podcast, Request $request ,  PodcastRepository $podcastRepository): JsonResponse
+    {
+        $podcast = $podcastRepository->find($podcast);
+        $podcastRepository->remove($podcast,true);
+
+        return $this->json([
+             'data'=>$podcast
+            ]);
     }
 }
